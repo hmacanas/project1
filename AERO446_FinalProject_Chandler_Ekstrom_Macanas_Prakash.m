43ad2ecbdc69d346a9fb27b5950769b161fa5522
Eric Ekstrom <eaekstro@calpoly.edu>
2018-06-06 14:52:31 -0700

    added moon orbital parameters

--- AERO446_FinalProject_Chandler_Ekstrom_Macanas_Prakash.m
+++ AERO446_FinalProject_Chandler_Ekstrom_Macanas_Prakash.m
@@ -11,10 +11,11 @@ clc
 
 % Constants
 r_earth = 6378; % radius of earth, [km]
+r_moon = 1737; % radius of moon, [km]
 mu_earth = 398600; % grativational parameter of earth, [km^3 / s^2]
-
+mu_moon = 4904.87; % grativational parameter of moon, [km^3 / s^2]
 % Givens
-data_sensor = 100; % data sensor produces, [kbps]
+dataRate_sensor = 100; % data sensor produces, [kbps]
 P_sensor = 40; % sensor power consumed, [W]
 P_standby = 2; % sensor on standby power consumed, [W]
 z = 200; % altitude, [km]
@@ -27,8 +28,15 @@ margin = 5; % link budget margin, [dB]
 % Low Cost
 
 
+% orbital parameters for the moon
+alt = 200; % altitude of orbit, [km]
+T = 2*pi*sqrt((r_moon + alt)^3/mu_moon); % Period of orbit, [s]
+data = dataRate_sensor*T/1000; % data per day required to send [Mb]
 
-
+ang = 2*(90 - acosd(r_moon/(r_moon + alt))); % angle for arc in eclipse, [deg]
+tEclipse = T*ang/360; % time spent in eclipse, [s]
+tSight = T - tEclipse; % time spent in [Earth line-of-sight / Sunlight], [s]
+numOrbits = floor(24*60*60/T); % number of orbits around moon in a day
 
 
 
