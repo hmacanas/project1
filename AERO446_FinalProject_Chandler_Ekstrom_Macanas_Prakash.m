d44343e5c259a93a5a16e09c09e1fab0120fb121
deeksharp <39920191+deeksharp@users.noreply.github.com>
2018-06-06 09:26:23 -0700

    updated givens

--- AERO446_FinalProject_Chandler_Ekstrom_Macanas_Prakash.m
+++ AERO446_FinalProject_Chandler_Ekstrom_Macanas_Prakash.m
@@ -9,12 +9,22 @@ close all
 clear
 clc
 
+% Constants
+r_earth = 6378; % radius of earth, [km]
+mu_earth = 398600; % grativational parameter of earth, [km^3 / s^2]
 
 % Givens
 data_sensor = 100; % data sensor produces, [kbps]
 P_sensor = 40; % sensor power consumed, [W]
 P_standby = 2; % sensor on standby power consumed, [W]
 z = 200; % altitude, [km]
+t_trans = 2*60*60; % transmit time of s/c for 1 day, [s]
+d_ground = 20; % diameter of ground antenna, [m]
+T_noise = 150; % comms system temp noise, [K]
+T_sp = 40; % solar panel temperature (both), [C]
+losses = 5; % overall losseson comms systems, [dB]
+margin = 5; % link budget margin, [dB]
+% Low Cost
 
-%% Deeksha is buuuunk
+
 
