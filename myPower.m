e0bf0f49b15c87ccd7bdd6d4d9ce8a073f71d27a
Henry Macanas <henry@Henrys-MacBook-Pro.local>
2018-06-09 18:41:34 -0700

    myPower()-for eric

--- myPower.m
+++ myPower.m
@@ -1,4 +1,4 @@
-function [total_energy_science,total_energy_tracking,energy_total_body] = myPower()
+function [total_energy_science,total_energy_tracking,energy_total_body,energy_total_45] = myPower()
 % Constants
 r_moon = 1737; % radius of moon, [km]
 mu_moon = 4904.87; % grativational parameter of moon, [km^3 / s^2]
@@ -10,33 +10,29 @@ T = 2*pi*sqrt((r_moon + alt)^3/mu_moon); % Period of orbit, [s]
 ang = 2*(90 - acosd(r_moon/(r_moon + alt))); % angle for arc in eclipse, [deg]
 tEclipse = T*ang/360; % time spent in eclipse, [s]
 
-%% Low Cost >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+%% Low Cost
 
 
-% needed parameters
+% needed parameters>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 w = 2*pi/T;                                         % angular velocity rads/s
 Se = 1367;                                          % W/m^2
 Temp = 40;                                          % nominal solar panel temp
 Temp_ref = 28;                                      % solar panel reference temp
-
-% NEED TO CHANGE
 Area = .3*.2;                                       % area of each panel
 
-% efficiency
-
-% NEED TO CHANGE THESE 
+% efficiency>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 e_cell = .29;                                       % solar cell efficiency
 e_deg = .025;                                       % cell degradation/year
 e_temp = .005;                                      % temp efficiency change
-
-% THESE ARE FINE 
+ 
 E_temp = e_cell*(1-e_temp*(Temp-Temp_ref));         % temperature effciency
 E_time = (1-e_deg)^7;                               % time efficiency
 E_packing_desnity = 1;                              % packing desnity for now
 E_total = E_temp*E_time*E_packing_desnity;          % combined efficiency
 
-% power calcs when collecting science (solar panels at a 45)
+%% Science orientation
+% power calcs when collecting science (solar panels at a 45)>>>>>>>>>>>>>>>
 syms t
 theta = w*t;                                        % angle of incidence 
 Area_wetted = 2*cos(pi/4)*Area;
@@ -46,24 +42,30 @@ P_side1 = 0;                                        % power side panel 1
 P_side2 = 0;                                        % power side panel 1
 P_total_science = P_fore_science+P_aft_science;                     % watts
 
-% energy calcs when collecting science
+% energy calcs when collecting science>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 energy_fore_science = double(int(P_fore_science,t,[tEclipse/2, T/2]));      % energy produced by fore
 energy_aft_science = double(int(P_aft_science,t,[T/2, T - tEclipse/2]));    % energy produced by aft
 total_energy_science = (energy_fore_science + energy_aft_science)/3600;     %(whrs)
-
-% power calcs when colecting power
+%% 3-body
+% power calcs when colecting power>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 syms t
 P_fore_body = Se*Area_wetted*E_total*sin(theta);                 % power fore panel
 P_aft_body =  Se*Area_wetted*E_total*-sin(theta);                % power aft panel
 P_zenith_body = Se*Area*E_total*-cos(theta);                     % zenith panel power
 P_total_body = P_fore_body+P_aft_body+P_zenith_body;
 
-% energy calcs 
+% energy calcs >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 energy_fore_body = double(int(P_fore_body,t,[tEclipse/2, T/2]));
 energy_aft_body = double(int(P_aft_body,t,[T/2, T - tEclipse/2]));
-energy_zenith_body = double(int(P_zenith_body,t,[tEclipse/4, 3*tEclipse/4]));
+energy_zenith_body = double(int(P_zenith_body,t,[T/4, 3*T/4]));
 energy_total_body = (energy_fore_body + energy_aft_body + energy_zenith_body)/3600;
 
+%% 45 angle always
+% power
+P_45_all = Se*Area_wetted*E_total*sin(pi/2);
+
+% energy
+energy_total_45 = (double(int(P_45_all,t,[0, T-tEclipse])))/3600;
 % Comms>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 %% High Cost
