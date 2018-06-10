2af8576ee68338f6e6c116e1b2cb9f2c7f9a7340
Henry Macanas <henry@Henrys-MacBook-Pro.local>
2018-06-10 15:37:38 -0700

    calculates how much time we need to transmit per orbit

---
+++ transmit_time.m
@@ -0,0 +1,17 @@
+function [time] = transmit_time(R,data,numOrbits)
+
+    % Input:
+        % max data rate to close the link in (bits/s)
+        % amount of data needed to to trasnmitted per day (data/day)
+    % Output:
+        % time needed to trasnmit per orbit
+ 
+    % amount of data per orbit needed to send
+    data_per_orbit = data/numOrbits;
+    
+    % calculate amount of time 
+    time = data_per_orbit/R;
+    
+    
+end
+
