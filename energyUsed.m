97311a065ad8a07c69c6b113e83bdd29eaa1ce5b
Lea Chandler <lea.a.chandler@gmail.com>
2018-06-10 17:55:54 -0700

    added comments

--- energyUsed.m
+++ energyUsed.m
@@ -1,17 +1,26 @@
 function [ E ] = energyUsed(t_transmit, t_science, period)
-% t in seconds
-% E in W
+% Function to calculate energy used by spacecraft operations over some
+% period
+%
+% Inputs:   t_transmit: time the sapcecraft is transmitting [s]
+%           t_science: time spacecraft is collecting science [s]
+%           period: total time of interest [s]
+% Outputs:  E: total energy used by the spacecraft [W-hr]
 
-t_idle = period - t_transmit - t_science;   % [s]
-
+% Constant: found in duty cycle table
 P_transmit = 41.725;    % [W]
 P_science = 57.325;     % [W]
 P_idle = 19.325;        % [W]
 
+% idle time
+t_idle = period - t_transmit - t_science;   % [s]
+
+% energy used in each mode
 E_transmit = t_transmit * P_transmit / 60;	% [W-hrs]
 E_science = t_science * P_science / 60;     % [W-hrs]
 E_idle = t_idle * P_idle / 60;              % [W-hrs]
 
+% total energy
 E = E_transmit + E_science + E_idle;        % [W-hrs]
 
 end
