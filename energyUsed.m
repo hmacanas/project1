2cb6b0caa799a95e783fc24eac509edab4ddc2cd
Lea Chandler <lea.a.chandler@gmail.com>
2018-06-10 20:00:11 -0700

    updated to include eclipse

--- energyUsed.m
+++ energyUsed.m
@@ -1,31 +1,40 @@
-function [ E ] = energyUsed(t_transmit, t_science, period)
+function [ E, E_transmit, E_eclipse ] = ...
+    energyUsed(t_transmit, t_science, t_eclipse, period)
+
 % Function to calculate energy used by spacecraft operations over some
 % period
 %
 % Example usage
 % energyUsed(15*60, T, 45*60*10, T*11)
 % where T is lunar orbit period
-%       45 is eclipse time per lunar orbit in minutes
+%       (15*60) seconds of transmitting
+%       (45*60)*(10periods) seconds of non-science eclipse
+%       (T*11periods) seconds total for time span of interest
 %
-% Inputs:   t_transmit: time the sapcecraft is transmitting [s]
+% Inputs    t_transmit: time the sapcecraft is transmitting [s]
 %           t_science: time spacecraft is collecting science [s]
+%           t_eclipse: time spacecraft is in eclipse [s]
 %           period: total time of interest [s]
-% Outputs:  E: total energy used by the spacecraft [W-hr]
+% Outputs   E: total energy used including margin [W-hr]
+%           E_transmit: energy used by the during transmit [W-hr]
+%           E_eclipse: energy used by the during eclipse [W-hr]
 
-% Constant: found in duty cycle table
-P_transmit = 41.725;    % [W]
-P_science = 57.325;     % [W]
-P_idle = 19.325;        % [W]
+% Constant: found in duty cycle table [W]
+P_transmit = 40.6;
+P_science = 56.2;
+P_idle = 18.2;
+P_eclipse = P_idle;
 
-% idle time
-t_idle = period - t_transmit - t_science;       % [s]
+% idle time [s]
+t_idle = period - t_transmit - t_science - t_eclipse;
 
-% energy used in each mode
-E_transmit = t_transmit * P_transmit / 3600;	% [W-hrs]
-E_science = t_science * P_science / 3600;       % [W-hrs]
-E_idle = t_idle * P_idle / 3600;                % [W-hrs]
+% energy used in each mode [W-hrs]
+E_transmit = t_transmit * P_transmit / 3600;
+E_science = t_science * P_science / 3600;
+E_idle = t_idle * P_idle / 3600;
+E_eclipse = t_eclipse * P_eclipse / 3600;
 
-% total energy
-E = E_transmit + E_science + E_idle;            % [W-hrs]
+% total energy including margin [W-hrs]
+E = (E_transmit + E_science + E_idle + E_eclipse) * 1.2;
 
 end
